29b456194ba895dfdb1ec7c587a29c08337de16d
annmaryvinod <annmarypriyavinod@gmail.com>
2023-05-23 15:36:54 +0530

    added some documentation

--- project1/server.js
+++ project1/server.js
@@ -3,3 +3,9 @@
 const express = require("express"); //requiring express
 const app = express(); 
 app.listen(3000,()=>{console.log("Server running on port 3000")}); //specifying the port number
+
+
+//Run this and go to localhost:3000
+// An error is thrown : "Cannot GET/"
+// This means we could not get the reponse from the root(/)
+// To get over this look at project2
